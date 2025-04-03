2858b7a30b3c1e697adaacb80fd75760e3c6d65b
David Mireles <a00836010@tec.mx>
2025-04-03 17:22:25 -0600

    pushing deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { SignIn } from "./Pages/SignIn";
 import { Dashboard } from "./Pages/Dashboard";
 import { Contact } from "./Pages/Contact";
